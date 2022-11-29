3f70c2d79118907d70cd85f52be127e4c0e7b0a0
Zack Ross <zmr462@usask.ca>
2022-11-29 14:50:15 -0600

    feat(backend): integrate with firebase
    Integrate the voicemail-service with Google firebase:
    - save messages to Firestore
    - send messages to devices via FCM
    
    Send messages via FCM removes the need for RabbitMQ and custom protobuf
    messages.
    
    Update documentation to reflect code changes

--- voicemail-service/README.rst
+++ voicemail-service/README.rst
@@ -5,27 +5,33 @@ Voicemail Service
 
 A simple voicemail service using Signalwire's RELAY SDK.
 
-This service handles calls to voicemail, transcribes recorded messages, and publishes the result
-to the RabbitMQ broker.
+This service handles calls to voicemail, transcribes recorded messages, saves the data to the
+database and sends a notification via Firebase Cloud Messaging.
 
 
-Usage
--------------------
+----------------------
+Installation & Usage
+----------------------
 
 The use of a virtual environment is highly recommended.
 
 .. code-block:: sh
 
     sudo apt-get update
-    sudo apt-get install -y libsndfile1 ffmpeg protobuf-compiler
-
-    # Protobuf codegen
-    protoc -I../protobuf --python_out=./voicemail/ ../protobuf/voicemail.proto
+    sudo apt-get install -y libsndfile1 ffmpeg
 
     # Install and build the project
     pip install Cython
     pip install ./
 
     # Run the voicemail service
-    voicemail-service $SIGNALWIRE_PROJECT $SIGNALWIRE_API_TOKEN --amqp_host $RABBITMQ_HOST
+    voicemail-service $SIGNALWIRE_PROJECT $SIGNALWIRE_API_TOKEN $CMS_URL --firebase_cert ./firebase_cert.json
 
+
+`voicemail-service` requires that the following files are accessible at `$CMS_URL`:
+
+* `/leave_message.mp3`
+* `/message_recorded.mp3`
+* `/time_limit.mp3`
+
+These mp3 files are played to provide feedback to the caller, and prompt for actions.
--- voicemail-service/voicemail/service.py
+++ voicemail-service/voicemail/service.py
@@ -1,15 +1,16 @@
-import logging, asyncio, uuid
+import logging, asyncio
 from datetime import datetime, timezone
-
+from signalwire.relay import event
 from signalwire.relay.consumer import Consumer
 from signalwire.relay.calling import Call
-from signalwire.relay.calling.constants import MediaType, CallRecordState
-from kombu import Connection, Queue, Exchange
+from signalwire.relay.calling.call import RecordAction
+from signalwire.relay.calling.constants import CallState
+import nanoid
+import firebase_admin
+from firebase_admin import credentials, firestore, messaging
 
-import voicemail.voicemail_pb2 as proto
 
-
-MAX_MSG_DURATION = 120
+MAX_MSG_DURATION = 180
 
 logging.basicConfig(
     level=logging.INFO,
@@ -18,76 +19,92 @@ logging.basicConfig(
     style='{')
 
 
-class VoicemailConsumer(Consumer):
+class VoicemailService(Consumer):
     """
     The voicemail consumer. This service handles calls received to a voicemail number.
     """
     def __init__(self, **kwargs):
         super().__init__()
-        from voicemail.asr import ASRModel
-
         # Required by RELAY
         self.project = kwargs["project"]
         self.token = kwargs["token"]
         self.contexts = ["voicemail"]
+        # Other variables
+        self.cms_url = kwargs["cms_url"]
 
-        self.language = "en-CA"
+        from voicemail.asr import ASRModel
         self.asr = ASRModel()
-        self.amqp_host = kwargs["amqp_host"]
+        logging.info(f"{self}: ASR Model initialized.")
+
+        self.firebase = firebase_admin.initialize_app(credentials.Certificate(kwargs["firebase_cert"]))
+        self.firestore = firestore.client(self.firebase)
+        logging.info(f"{self}: Firebase initialized.")
 
     async def on_incoming_call(self, call: Call):
         """
         Handles incoming calls the voicemail consumer.
         """
-        logging.info("Call Received: {} to {}".format(call.from_number, call.to_number))
+        logging.info(f"[{call.id}] Receiving incoming call")
 
-        voicemail_queue = Queue(call.to_number, Exchange('voicemail'), routing_key=call.to_number)
+        users_ref = self.firestore.collection("users")
+        user = (users_ref.where("voicemail_number", "==", call.to_number).get()[0:1] or [None])[0]
 
         result = await call.answer()
         if result.successful:
-            await call.play([
-                {"type": MediaType.SILENCE, "duration": 1 },
-                {
-                    "type": MediaType.TTS,
-                    "language": self.language,
-                    "text": "The person you are calling is currently unavailable; please leave a message after the tone. " +
-                            "To finish recording, you may hang up, or press pound."
-                },
-            ])
+            logging.info(f"[{call.id}] Answered call")
+            await call.play_silence(1)
+            await call.play_audio(f"{self.cms_url}/leave_message.mp3", volume=16)
+
             recording = await call.record_async(beep=True, direction="speak", record_format="mp3")
-            for _ in range(MAX_MSG_DURATION):
-                if recording.completed:
-                    await recording.stop()
-                    if recording.result.successful and recording.state == CallRecordState.FINISHED:
-                        logging.info("Call Recorded:{} to {}".format(call.from_number, call.to_number))
 
-                        transcription = self.asr.transcribe("https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg.wav")
-
-                        msg = proto.Voicemail()
-                        msg.to_number = call.to_number
-                        msg.from_number = call.from_number
-                        msg.audio_url = recording.url
-                        msg.transcription = transcription
-                        msg.timestamp.FromDatetime(datetime.now(timezone.utc))
-
-                        with Connection(self.amqp_host) as connection:
-                            producer = connection.Producer(connection)
-                            producer.publish(msg.SerializeToString(),
-                                exchange=voicemail_queue.exchange,
-                                routing_key=call.to_number,
-                                declare=[voicemail_queue],
-                                retry=True)
-                    break
+            elapsed = 0
+            while (elapsed < MAX_MSG_DURATION and not recording.completed):
+                elapsed += 1
                 await asyncio.sleep(1)
 
-            if not recording.completed:
-                await recording.stop()
-                await call.play_tts(
-                    language=self.language,
-                    text="Time limit reached. Your message has been recorded, please call again later.")
+            await recording.stop()
 
-        await call.hangup()
-        logging.info("Call Ended: {} to {}".format(call.from_number, call.to_number))
+            if recording.result.successful:
+                logging.info(f"[{call.id}] Recorded voice message")
+                if elapsed >= MAX_MSG_DURATION:
+                    await call.play_audio(f"{self.cms_url}/time_limit.mp3", volume=16)
+                await call.play_audio(f"{self.cms_url}/message_recorded.mp3", volume=16)
+                await call.hangup()
+                logging.info(f"[{call.id}] Ended call")
+                self._on_successful_recording(user, call, recording)
+            else:
+                await call.hangup()
+                logging.info(f"[{call.id}] Ended call")
+        else:
+            logging.error(f"[{call.id}] Could not answer incoming call")
+
+    def _on_successful_recording(self, user, call: Call, recording: RecordAction):
+        """
+        Handles when a voicemail message is successfully recorded.
+        """
+        transcription = self.asr.transcribe(recording.url)
+
+        msg_id = nanoid.generate()
+        msg_data = {
+            "from_number": call.from_number,
+            "audio_url": recording.url,
+            "transcription": transcription,
+            "timestamp": datetime.now(timezone.utc)
+        }
+        try:
+            messages_ref = self.firestore.collection("users").document(user.id).collection("messages")
+            messages_ref.document(msg_id).set(msg_data)
+        except Exception as err:
+            logging.error(f"[{call.id}] Could not save message: {err}")
+
+        msg = messaging.Message(
+            data={ "message_id": msg_id },
+            token=user.get("fcm_token"))
+
+        try:
+            messaging.send(msg, app=self.firebase)
+        except Exception as err:
+            logging.error(f"[{call.id}] Could not send message: {err}")
 
 
 def main():
@@ -98,14 +115,18 @@ def main():
 
     parser.add_argument("project", type=str, help="A SignalWire project ID")
     parser.add_argument("token", type=str, help="A SignalWire API token")
-    parser.add_argument("--amqp_host",
+    parser.add_argument("cms_url",
         type=str,
-        default="localhost",
-        help="The connection string of an AMQP 0-9-1 server (default: 'localhost')")
+        help="The content management system URL. This points to the location of the audio prompts, "
+            "and must be accessible by SignalWire. i.e., not localhost")
+    parser.add_argument("--firebase_cert",
+        type=str,
+        default="./service_cert.json",
+        help="The Firebase service account key path (default: './service_cert.json')")
 
     args = parser.parse_args()
 
-    voicemail_consumer = VoicemailConsumer(**vars(args))
+    voicemail_consumer = VoicemailService(**vars(args))
     voicemail_consumer.run()
 
 
